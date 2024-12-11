09f39892eea156b50ab3f1db9d4565c6ab57889b
ir <Isabelaruiz147@gmail.com>
2024-12-11 11:02:57 -0500

    Test

---
+++ Files/write_message.py
@@ -0,0 +1,6 @@
+filename = '/Users/isabela/Desktop/Python/Files/programming.txt'
+
+with open(filename, 'w') as file_object:
+    file_object.write("I love programming. \n")
+    file_object.write("I love creating new games. \n")
+
